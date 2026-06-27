ffa2f564d6b7458e52e912c0efb337148d8c6c82
Reinis Ikass <reinis.ikass@gmail.com>
2026-06-27 12:02:57 +0300

    Add SupportController, set root to static#home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "support", to: "support#show"
   get "activity/mine"
   get "activity/feed"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,5 +13,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "activity#mine"
+  root "static#home"
 end
